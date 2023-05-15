a56c02d0cd30d6e19dc6179160f13aa344bee15c
motsu8e <motsu159357@gmail.com>
2023-05-16 01:21:50 +0900

    core/ram描画

--- main.js
+++ main.js
@@ -22,6 +22,46 @@ class Parts {
     }
 }
 
+class Ram extends Parts{
+    constructor(parts){
+        super(parts)
+        this.keys = new Map();
+        this.sortList = []
+    }
+
+    init(){
+        this.getKeys();
+        this.getSortList();
+    }
+
+    getKeys(){
+        for(const ele of this.list){
+            const model = this.getSpec(ele.Model)
+            if(!this.keys.has(model)){
+                this.keys.set(model, [ele])
+            }else{
+                this.keys.set(model, [...this.keys.get(model), ele])
+            }
+        }
+    }
+
+    // 最後のスペック取得
+    getSpec(model){
+        return parseInt(model.split(" ").pop()[0])
+    }
+
+    // sortList
+    getSortList(){
+        // 重複なしリスト
+        const amountList = new Set();
+        for(const ramEle of this.list){
+            amountList.add(this.getSpec(ramEle.Model))
+        }
+        this.sortList = [...amountList]
+        this.sortList.sort((a, b)=>a-b)
+    }
+}
+
 class Storage extends Parts{
     constructor(parts){
         super(parts)
@@ -34,16 +74,16 @@ class Storage extends Parts{
 
 class View{
     constructor(){
-        this.parts = [
+        this.types = [
             new Parts("cpu"),
             new Parts("gpu"),
-            new Parts("ram"),
+            new Ram("ram"),
             new Storage("storage"),
         ];
     }
 
     init(){
-        for(const ele of this.parts){
+        for(const ele of this.types){
             const container = document.createElement("div");
             container.innerHTML = `
                 <h3>Select Your ${ele.parts.toUpperCase()}</h3>
@@ -61,7 +101,7 @@ class View{
                 case"ram":
                     container.innerHTML += `
                     <label for="step-2">How many?</label>
-                    <select name="" id="step-2" class="col-3"></select>
+                    <select name="" id="step-2" class="amount col-3"></select>
                     `;
                     break;
             }
@@ -86,31 +126,53 @@ class View{
 class Ctrl{
     constructor(){}
 
-    // Brand描画
-    static setBrand(view){
-        for(const parts of view.parts){
-            // 重複なしリスト
-            const brandList = new Set();
-            for(const type of parts.list){
-                brandList.add(type.Brand)
-            }
+    // Ramの描画
+    static setRam(view){
+        // ram取得
+        const ram = view.types.filter(e=>e.parts == "ram").pop()
+        ram.init();
+        
+        // amount描画
+        const amountSelectTag = ram.element.querySelectorAll(".amount")[0];
+        ram.sortList.forEach(e=>{
+            const option = document.createElement("option");
+            option.value = e;
+            option.innerHTML = e;
+            amountSelectTag.append(option);
+        })
 
-            // brand表示
-            const brandEle = parts.element.querySelectorAll(".brand")[0];
-            brandList.forEach(e=>{
-                const option = document.createElement("option");
-                option.value = e;
-                option.innerHTML = e;
-                brandEle.append(option)
-            })
-        }
+        amountSelectTag.addEventListener('change', e=>{
+            // setBrand
+            console.log(e.target.value)
+        })
     }
 
+    // CPU | GPU
+    static setCore(view){
+        for(const ele of view.types){
+            if(ele.parts == "cpu" || ele.parts == "gpu"){
+                // 重複なしリスト
+                const brandList = new Set();
+                for(const type of ele.list){
+                    brandList.add(type.Brand)
+                }
+                // brand表示
+                const brandSelectTag = ele.element.querySelectorAll(".brand")[0];
+                brandList.forEach(e=>{
+                    const option = document.createElement("option");
+                    option.value = e;
+                    option.innerHTML = e;
+                    brandSelectTag.append(option)
+                })
+            }
+        }
+    }
 }
 
 const view = new View();
 view.init()
 
 setTimeout(() => {
-    Ctrl.setBrand(view)
-}, 50);
+    Ctrl.setCore(view)
+    Ctrl.setRam(view)
+}, 100);
